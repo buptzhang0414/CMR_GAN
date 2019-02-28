d9b2e079423d7288178dd2bd9de53c4ff4b978e4
buptzhang0414 <37605718+buptzhang0414@users.noreply.github.com>
2019-02-28 23:48:24 +0800

    Add files via upload

---
+++ util/utils_mkdir.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+
+import os
+import json
+import logging
+import numpy as np
+from datetime import datetime
+
+def prepare_dirs_and_logger(config):
+    formatter = logging.Formatter("%(asctime)s:%(levelname)s::%(message)s")
+    logger = logging.getLogger()
+
+    for hdlr in logger.handlers:
+        logger.removeHandler(hdlr)
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+    config.model_name = "{}_{}".format(config.dataset, get_time())
+
+    if not hasattr(config, 'model_dir'):
+        config.model_dir = os.path.join(config.log_dir, config.model_name)
+
+    for path in [config.log_dir, config.model_dir]:
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+def get_time():
+    return datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+def save_config(config):
+    param_path = os.path.join(config.model_dir, "params.json")
+
+    print("[*] MODEL dir: %s" % config.model_dir)
+    print("[*] PARAM path: %s" % param_path)
+
+    with open(param_path, 'w') as fp:
+        json.dump(config.__dict__, fp, indent=4, sort_keys=True)
